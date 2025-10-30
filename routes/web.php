522730011744ca100436bb139db757ef05617c87
DGT_LamCM <lam.cao@digitran.asia>
2025-10-30 19:35:58 +0700

    Test new route

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/hello', function() {
-    return view('hello');
-});
+Route::get('/hello', [HelloController::class, 'index']);
