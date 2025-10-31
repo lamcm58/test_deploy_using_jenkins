5ccf4e05f3010c6273ab137cf45c9ed61fe55ba5
DGT_LamCM <lam.cao@digitran.asia>
2025-10-31 16:59:33 +0700

    Update encrypt type - v3

--- ansible/vault_password.sh
+++ ansible/vault_password.sh
@@ -4,5 +4,13 @@
 
 # Read vault password from Jenkins credential
 # This file should be in Jenkins Credentials and passed as environment variable
+
+# Check if environment variable is set
+if [ -z "${ANSIBLE_VAULT_PASSWORD}" ]; then
+    echo "ERROR: ANSIBLE_VAULT_PASSWORD environment variable is not set" >&2
+    exit 1
+fi
+
+# Output the vault password (Ansible will read this)
 echo "${ANSIBLE_VAULT_PASSWORD}"
 
